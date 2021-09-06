227207f20db800d0fb19f2bffa86a50902a44060
Gleibson Alves <alvesgleibson@gmail.com>
2021-09-06 16:54:52 -0300

    Saving a new Department

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,17 +3,24 @@ package gui;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import db.DbException;
+import gui.util.Alerts;
 import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable {
 
 	private Department departmentEntity;
+	private DepartmentService service;
 
 	@FXML
 	private TextField txtId;
@@ -32,14 +39,39 @@ public class DepartmentFormController implements Initializable {
 		this.departmentEntity = departmentEntity;
 	}
 
-	@FXML
-	public void onbtSaveAction() {
-		System.out.println("onbtSaveAction");
+	public void setDepartmentServe(DepartmentService service) {
+		this.service = service;
 	}
 
 	@FXML
-	public void onBtCancelAction() {
-		System.out.println("onBtCancelAction");
+	public void onbtSaveAction(ActionEvent event) {
+		if (departmentEntity == null) {
+			throw new IllegalAccessError("Department was Null");
+		}
+		if (service == null) {
+			throw new IllegalAccessError("DepartmentService was null");
+		}
+		try {
+			departmentEntity = getFormDate();
+			service.saveOrUpdate(departmentEntity);
+			Utils.currentScene(event).close();
+			updateFormDateDepertment();
+			
+			
+		} catch (DbException e) {
+			Alerts.showAlert("Error saving Object", null, e.getMessage(), AlertType.ERROR);
+		}
+		
+
+	}
+
+	private Department getFormDate() {
+		return new Department(Utils.tryParseToInt(txtId.getText()), txtName.getText());
+	}
+
+	@FXML
+	public void onBtCancelAction(ActionEvent event) {
+		Utils.currentScene(event).close();
 	}
 
 	@Override
--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -5,11 +5,18 @@ import javafx.scene.Node;
 import javafx.stage.Stage;
 
 public class Utils {
-	
-	
+
 	public static Stage currentScene(ActionEvent event) {
 		return (Stage) ((Node) event.getSource()).getScene().getWindow();
 	}
-	
+
+	public static Integer tryParseToInt(String msg) {
+		try {
+			return Integer.parseInt(msg);
+		} catch (NumberFormatException e) {
+			return null;
+		}
+
+	}
 
 }
